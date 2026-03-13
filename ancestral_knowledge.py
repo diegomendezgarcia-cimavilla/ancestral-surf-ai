5b11356d9a6995fb3128d1549ba8035b87efedd9
diegomendezgarcia-cimavilla <diegomendezgarcia@gmail.com>
2026-03-13 23:12:43 +0100

    Create ancestral_knowledge.py

---
+++ ancestral_knowledge.py
@@ -0,0 +1,32 @@
+knowledge = {
+
+"low_energy":[
+"comer sopa caliente de verduras",
+"descansar 20 minutos",
+"caminar por la playa",
+"evitar cannabis temprano",
+"tomar infusión de jengibre"
+],
+
+"high_stress":[
+"respiración profunda 10 minutos",
+"surf relajado",
+"infusión de manzanilla",
+"escribir pensamientos"
+],
+
+"good_energy":[
+"surf largo",
+"trabajo físico moderado",
+"pescado fresco y arroz",
+"actividad creativa"
+],
+
+"creative_state":[
+"dibujar o escribir ideas",
+"música tranquila",
+"paseo largo",
+"consumo consciente de cannabis"
+]
+
+}
